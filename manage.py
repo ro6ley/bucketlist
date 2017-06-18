b084aeb062d11a8dbd083e55b980d228019791f7
Robley Gori <robleyadrian@gmail.com>
2017-06-18 16:10:01 +0300

    [CHANGE] Change database to postgres from sqlite

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app import db, create_app
-from app import models
+from app.app import db, create_app
+from app.models import BucketList, Item, User
 
 
 app = create_app(config_name="development")
@@ -12,3 +12,4 @@ manager.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
+    db.create_all()
