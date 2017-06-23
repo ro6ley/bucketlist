7f1d81cf5216a12e58dd18e8555fe27557bbde89
Robley Gori <robleyadrian@gmail.com>
2017-06-23 15:59:53 +0300

    [CHANGE] Remove commented code

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 #!./bl-env/bin/python
 
-import os
-
-# from app.views import create_app
 from app.app import create_app
 
 config_name = "development"
-# config_name = os.getenv('APP_SETTINGS')
 app = create_app("development")
 
 if __name__ == "__main__":
